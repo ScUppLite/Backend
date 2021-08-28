7439fc295a7bbc0c3f544697fc9464bc988055ed
pinhobrunodev <iaishowcs@gmail.com>
2021-08-28 17:40:48 -0300

    first commit

---
+++ src/main/java/com/scupp/scupplite/ScuppliteApplication.java
@@ -0,0 +1,13 @@
+package com.scupp.scupplite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScuppliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScuppliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scupp/scupplite/ScuppliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scupp.scupplite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScuppliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
